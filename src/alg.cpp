c1d641becc6506f14180054e1ac23e9b852cb7c4
abramovayulia <116936584+abramovayulia@users.noreply.github.com>
2023-03-11 23:41:22 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,8 +1,6 @@
 // Copyright 2022 NNTU-CS
 #include <cstdint>
 #include "alg.h"
-#include <iostream>
-using namespace std;
 
 double pown(double value, uint16_t n) {
 	if (n == 0) {
@@ -41,23 +39,10 @@ double sinn(double x, uint16_t count) {
 	return sum;
 }
 
-ddouble cosn(double x, uint16_t count) {
+double cosn(double x, uint16_t count) {
 	double sum = 0;
 	for (count; count > 0; count--) {
 		sum += pown(-1, count-1) * calcItem(x, 2*count-2);
 	}
 	return sum;
-}
-
-
-int main() {
-	setlocale(LC_ALL, "RU");
-	double value;
-	uint16_t n = 0;
-	cin >> value >> n;
-	cout << "Степень числа " << value << " = " << pown(value, n) << endl;
-	cout << "Факториал " << n << " = " << fact(n) << endl;
-	cout << "Экспонента " << " = " << expn(value, n) << endl;
-	cout << "Синус " << " = " << sinn(value, n) << endl;
-	cout << "Косинус " << " = " << cosn(value, n) << endl;
 }
