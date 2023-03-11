0dce4ff14a680ba8fc0b8e45cb5aa69500336fb9
abramovayulia <116936584+abramovayulia@users.noreply.github.com>
2023-03-11 22:41:14 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,8 +4,8 @@
 
 
 double pown(double value, uint16_t n) {
-  	if (n == 0) {
-      return 1;
+	if (n == 0) {
+		return 1;
 	}
 	else {
 		return value * pown(value, n-1);
@@ -13,7 +13,7 @@ double pown(double value, uint16_t n) {
 }
 
 uint64_t fact(uint16_t n) {
-  	if ((n == 1) || (n == 0)) {
+	if ((n == 1) || (n == 0)) {
 		return 1;
 	} else {
 		return n * fact(n - 1);
@@ -21,11 +21,11 @@ uint64_t fact(uint16_t n) {
 }
 
 double calcItem(double x, uint16_t n) {
-  	return (pown(x, n) / fact(n));
+	return (pown(x, n) / fact(n));
 }
 
 double expn(double x, uint16_t count) {
-  	double sum = 1.0;
+	double sum = 1.0;
 	for (count; count > 0; count--) {
 		sum += calcItem(x,count);
 	}
@@ -33,17 +33,30 @@ double expn(double x, uint16_t count) {
 }
 
 double sinn(double x, uint16_t count) {
-  	double sum = 0;
+	double sum = 0;
 	for (count; count > 0; count--) {
 		sum += pown(-1, count-1) * calcItem(x, 2*count-1);
 	}
 	return sum;
 }
 
-double cosn(double x, uint16_t count) {
-  	double sum = 0;
+ddouble cosn(double x, uint16_t count) {
+	double sum = 0;
 	for (count; count > 0; count--) {
 		sum += pown(-1, count-1) * calcItem(x, 2*count-2);
 	}
 	return sum;
+}
+
+
+int main() {
+	setlocale(LC_ALL, "RU");
+	double value;
+	uint16_t n = 0;
+	cin >> value >> n;
+	cout << "Степень числа " << value << " = " << pown(value, n) << endl;
+	cout << "Факториал " << n << " = " << fact(n) << endl;
+	cout << "Экспонента " << " = " << expn(value, n) << endl;
+	cout << "Синус " << " = " << sinn(value, n) << endl;
+	cout << "Косинус " << " = " << cosn(value, n) << endl;
 }
